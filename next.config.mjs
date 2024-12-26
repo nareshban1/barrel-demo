2856e6a377719a660a8a8e3cea0d70839abe24c7
Naresh Ban <nareshban40@gmail.com>
2024-12-26 11:13:33 +0545

    Add bundle analyzer configuration and update toast hook for improved functionality

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
+import withBundleAnalyzer from "@next/bundle-analyzer";
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+const config =
+  process.env.ANALYZE === "true"
+    ? withBundleAnalyzer({ enabled: true })(nextConfig)
+    : nextConfig;
+
+export default config;
